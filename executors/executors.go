d51b741393c5e6cfeb020966e7044d92c2912696
Riyaz Ali <me@riyazali.net>
2020-06-24 13:40:22 +0530

    ref: move executors into separate package

--- executors/executors.go
+++ executors/executors.go
@@ -1,19 +1,21 @@
-package httpx
+// Package executors provides default httpx.ExecFn based on Go standard library.
+package executors
 
 import (
+	"go.riyazali.net/httpx"
 	"net/http"
 	"net/http/httptest"
 	"time"
 )
 
 // WithDefaultClient wraps the http.DefaultClient into an ExecFn and returns it
-func WithDefaultClient() ExecFn {
+func WithDefaultClient() httpx.ExecFn {
 	return http.DefaultClient.Do
 }
 
 // WithClient returns an ExecFn that wraps an http.Client.
 // Use opts to customise the http.Client.
-func WithClient(opts ...func(*http.Client)) ExecFn {
+func WithClient(opts ...func(*http.Client)) httpx.ExecFn {
 	var client = &http.Client{}
 	for _, fn := range opts {
 		fn(client)
@@ -45,10 +47,17 @@ func WithTransport(t *http.Transport) func(*http.Client) {
 	}
 }
 
+// WithNoRedirect disables redirect on http.Client.
+func WithNoRedirect() func(*http.Client) {
+	return func(client *http.Client) {
+		client.CheckRedirect = func(*http.Request, []*http.Request) error { return http.ErrUseLastResponse }
+	}
+}
+
 // WithHandler wraps the given http.Handler and returns an ExecFn that invokes
 // the handler on request and return the response. This ExecFn doesn't need to make network round-trip
 // and can be used to implement unit tests for http endpoints in your application.
-func WithHandler(handler http.Handler) ExecFn {
+func WithHandler(handler http.Handler) httpx.ExecFn {
 	return func(request *http.Request) (*http.Response, error) {
 		var recorder = httptest.NewRecorder()
 		handler.ServeHTTP(recorder, request)
@@ -58,6 +67,6 @@ func WithHandler(handler http.Handler) ExecFn {
 
 // WithHandlerFn wraps the given http.HandlerFunc and returns an ExecFn.
 // See WithHandler(...) for more details.
-func WithHandlerFn(fn http.HandlerFunc) ExecFn {
+func WithHandlerFn(fn http.HandlerFunc) httpx.ExecFn {
 	return WithHandler(fn)
 }
